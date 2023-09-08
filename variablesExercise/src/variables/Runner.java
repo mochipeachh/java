582a013efd16d498543b79b7d90706b21d2b0a8f
lamees <lamees.khan9000@gmail.com>
2023-09-08 13:37:28 +0100

    variable exercises

---
+++ variablesExercise/src/variables/Runner.java
@@ -0,0 +1,19 @@
+package variables;
+
+public class Runner {
+    public static void main(String[] args) {
+//        String firstName = "Lamees";
+//        String lastName = "khan";
+//        String name = "Lamees " + "khan";
+//        System.out.println(name);
+        int num1 = 1;
+        int num2 = 2;
+        int sum  = num1 + num2;
+        System.out.println(sum);
+        float result = (float)num1/num2;
+        System.out.println(result);
+        int num3 = 8;
+        System.out.println(num3++);
+        System.out.println(num3);
+    }
+}
